1c7cf083169d5e5abfdf2e8bce34ebc3327ac25b
Ugge Karthik <157061823+karthikugge@users.noreply.github.com>
2026-07-20 13:40:33 +0530

    Time: 9 ms (98.92%) | Memory: 59.5 MB (36.95%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+
+    public ListNode sortList(ListNode head) {
+
+        // Base case
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Find middle
+        ListNode mid = getMid(head);
+
+        // Split the list
+        ListNode rightHead = mid.next;
+        mid.next = null;
+
+        // Recursively sort both halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightHead);
+
+        // Merge the sorted halves
+        return merge(left, right);
+    }
+
+    public ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+}
